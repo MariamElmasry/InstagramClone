45b5263d3e931723bb0a0d9e0697425951083d29
Mariam Elmasry <mariamelmasry312@gmail.com>
2019-04-19 22:54:21 +0200

    Tab layout set up

---
+++ app/src/main/java/com/example/mariam/instagramclone/TabAdapter.java
@@ -0,0 +1,52 @@
+package com.example.mariam.instagramclone;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class TabAdapter extends FragmentPagerAdapter {
+
+    public TabAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int tabPosition) {
+
+        switch (tabPosition){
+            case 0:
+                ProfileTab profileTab = new ProfileTab();
+                return profileTab;
+            case 1:
+                // ممكن تتكتب بالشكل ده بردو
+                return new UsersTab();
+            case 2:
+                SharePicture sharePicture = new SharePicture();
+                return sharePicture;
+                default:
+                    return null;
+        }
+
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position){
+            case 0:
+                return "Profile";
+            case 1:
+                return "Users";
+            case 2:
+                return "Share Pictures";
+                default:
+                    return null;
+        }
+    }
+}
--- app/src/main/java/com/example/mariam/instagramclone/WelcomeActivity.java
+++ app/src/main/java/com/example/mariam/instagramclone/WelcomeActivity.java
@@ -2,9 +2,18 @@ package com.example.mariam.instagramclone;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+    private androidx.appcompat.widget.Toolbar toolbar;
+    private ViewPager viewPager;
+    private TabLayout tabLayout ;
+    private TabAdapter tabAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +21,15 @@ public class WelcomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_welcome);
 
         setTitle("Welcome!!");
+
+        toolbar = findViewById(R.id.myToolbar);
+        setSupportActionBar(toolbar);
+
+        viewPager = findViewById(R.id.viewPager);
+        tabAdapter = new TabAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(tabAdapter);
+
+        tabLayout = findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager, false);
     }
 }
